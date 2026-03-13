6806ae118569375db5755cc951732d3e3746d238
ADiiiiABiiii <abhisuryakantbiradr@gmail.com>
2026-03-13 07:22:16 +0000

    solutions1 done

---
+++ Conditions/solutions1.py
@@ -0,0 +1,11 @@
+x=int(input("nimma age estri aunty "))
+if(x<=13):
+      print("ni child idi hogu magu")
+elif(13<x<=19):
+      print("ni teenger iddi avi")
+elif(19<=x<=59):
+      print("ni proper aunty iddi")
+elif(x>=60):
+      print("ni muduki iddi tooo")
+else:
+    print("shist age haku")
