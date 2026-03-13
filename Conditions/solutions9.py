b3689385aea3d700a5053b251341835949df174f
ADiiiiABiiii <abhisuryakantbiradr@gmail.com>
2026-03-13 07:15:36 +0000

    solutions9 solve done

---
+++ Conditions/solutions9.py
@@ -0,0 +1,5 @@
+x=int(input("enter your mann ka margi: "))
+if(x%4==0  and x%100!=0):
+    print("hurray your mann ka year is a leap year")
+else:
+    print("sorry your mann ka year is not a leap year")
