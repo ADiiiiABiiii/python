024746522161e76434a75e18afba2476fef72802
ADiiiiABiiii <abhisuryakantbiradr@gmail.com>
2026-03-13 07:37:14 +0000

    solutions3 done

---
+++ Conditions/solutions3.py
@@ -0,0 +1,8 @@
+d=int(input("ninu est marks bandav helu "))
+if(d<100):
+      if(d>=85):
+        print("bhari oo naveenaa ni distinction")
+      elif(85>d>40):
+        print("pass adi togo")
+      elif(d<40):
+        print("yappo fail adyoo naveenaaa")
